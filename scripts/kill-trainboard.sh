d901b8c3bc671e0b9aa26531daaf24ebb4c98662
Sam's Raspberry Pi <SamLeatherdale@users.noreply.github.com>
2020-02-08 22:57:45 +1100

    Add automatic startup and shutdown scripts

---
+++ scripts/kill-trainboard.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sleep 1 && xset -display :0.0 dpms force off && \ 
+/opt/vc/bin/tvservice -p && \ 
+/opt/vc/bin/tvservice -o
+killall chromium-browse
---
+++ scripts/launch-trainboard.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export DISPLAY=:0;
+/opt/vc/bin/tvservice -p && sleep 1 && \
+xset -display :0.0 dpms force on
+chromium-browser --kiosk "https://trainboard.netlify.com"
