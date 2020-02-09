2a88f2db6d4e80c23faa5344c3583d28d57d95f4
Sam Leatherdale <SamLeatherdale@users.noreply.github.com>
2020-02-09 11:22:05 +1100

    Switch back to using Node proxy

--- scripts/proxy.js
+++ scripts/proxy.js
@@ -1,15 +1,26 @@
+const fs = require('fs');
 const cors_proxy = require('cors-anywhere');
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Overwrite with local if exists
+const localPath = __dirname + '/../.env.local';
+if (fs.existsSync(localPath)) {
+    const localEnv = dotenv.parse(fs.readFileSync(localPath));
+    for (const k of Object.keys(localEnv)) {
+        process.env[k] = localEnv[k]
+    }
+}
 
 // Listen on a specific host via the HOST environment variable
-const host = '0.0.0.0';
+const host = process.env.PROXY_HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
-const port = 8010;
+const port = process.env.PROXY_PORT || 8010;
 
 cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
+    requireHeader: [],
+    removeHeaders: ['cookie', 'cookie2', 'origin']
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
